aa5165407e9394f2a8a6c7683fc807e8d936faa8
MOHAMMED REHAN <224443441+zfln-rehan0520@users.noreply.github.com>
2026-04-22 15:10:25 +0530

    storage.js

--- storage.js
+++ storage.js
@@ -1,35 +1,4 @@
-/**
- * storage.js - LocalStorage Management
- * General Purpose Edition
- */
-
 const VaultStorage = {
-    // We use a neutral key for general public use
-    KEY: 'vlt_encrypted_vault', 
-    
-    get() {
-        try {
-            const data = localStorage.getItem(this.KEY);
-            return data ? JSON.parse(data) : [];
-        } catch (e) {
-            console.error("Vault Error: Could not parse storage.", e);
-            return [];
-        }
-    },
-    
-    save(data) {
-        localStorage.setItem(this.KEY, JSON.stringify(data));
-    },
-    
-    add(entry) {
-        const db = this.get();
-        db.push(entry);
-        this.save(db);
-    },
-
-    deleteEntry(id) {
-        const db = this.get();
-        const filteredDb = db.filter(item => item.id != id);
-        this.save(filteredDb);
-    }
+    KEY: 'vlt_local_storage',
+    // ... paste get, save, add, and delete here
 };
